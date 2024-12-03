f63a81cc2685de6d7380dce579ee4b36fd6826d3
sraj39273@gmail.com <sraj39273@gmail.com>
2024-12-03 13:06:18 +0530

    done project

---
+++ movieUI/src/App.jsx
@@ -0,0 +1,81 @@
+import { useState } from 'react'
+import './App.css'
+
+const movies = [
+  { title: "The Shawshank Redemption", genre: "Drama", year: 1994 },
+  { title: "The Godfather", genre: "Crime", year: 1972 },
+  { title: "The Dark Knight", genre: "Action", year: 2008 },
+  { title: "12 Angry Men", genre: "Drama", year: 1957 },
+  { title: "Schindler's List", genre: "Drama", year: 1993 },
+  { title: "The Lord of the Rings: The Return of the King", genre: "Fantasy", year: 2003 },
+  { title: "The Good, the Bad and the Ugly", genre: "Western", year: 1966 },
+  { title: "Forrest Gump", genre: "Drama", year: 1994 },
+  { title: "Inception", genre: "Science Fiction", year: 2010 },
+  { title: "The Matrix", genre: "Science Fiction", year: 1999 },
+  { title: "The Silence of the Lambs", genre: "Thriller", year: 1991 },
+  { title: "Saving Private Ryan", genre: "War", year: 1998 },
+  { title: "Jurassic Park", genre: "Science Fiction", year: 1993 },
+  { title: "Terminator 2: Judgment Day", genre: "Science Fiction", year: 1991 },
+  { title: "The Lion King", genre: "Animation", year: 1994 }
+];
+
+const genres = [ "Drama", "Crime", "Action", "Fantasy", "Western", "Science Fiction", "Thriller", "War", "Animation", ];
+
+function App() {
+  const [filteredMovie,setFilteredMovie]=useState(movies);
+
+  const filterMovieByGenre=(x)=>{
+    let mov=[...filteredMovie];
+
+    console.log(`filtered item by ${x}`)
+
+    let newArr=mov.filter((item)=>{
+      
+      return item.genre===x
+    });
+
+    setFilteredMovie(newArr);
+  }
+
+  return (
+    <div className='container'>
+        <h1>Top 15 Movies of All Time</h1>
+
+        <div className='genreBox'>
+          <h3>Filter by genre</h3>
+          <div className='genreButtonContainer'>
+          {
+            genres.map((item,index)=>(
+              <button className='genreButton' key={index} onClick={()=>filterMovieByGenre(item)}>
+                {item}
+              </button>
+            ))
+          }
+          </div>
+        </div>
+        
+        <div className='movieBoxContainer'>
+            <table>
+              <thead>
+                <th className='title'>Title</th>
+                <th>Genre</th>
+                <th>Year</th>
+              </thead>
+              <tbody>
+                {
+                  movies.map((item,index)=>(
+                    <tr key={index}>
+                      <td className='itemTitle'>{item.title}</td>
+                      <td>{item.genre}</td>
+                      <td>{item.year}</td>
+                    </tr>
+                  ))
+                }
+              </tbody>
+            </table>
+        </div>
+    </div>
+  )
+}
+
+export default App
